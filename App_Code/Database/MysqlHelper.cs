9815f781008318dd58ad1aaec380595d77675268
LittleFrogFlaming <1511749834@qq.com>
2016-09-23 16:51:20 +0800

    完善数据库操作类
    连接库MysqlHelper和MysqlOperate

---
+++ App_Code/Database/MysqlHelper.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Mysql数据库类
+/// </summary>
+public class MysqlHelper
+{
+    //链接字符串
+    private static String constr = "server=localhost;user id=root;password=;database=bms;Persist Security Info=True;Charset=utf8;";
+    //数据库连接
+    private static MySqlConnection connection;
+
+    //建立数据库连接
+    public static MySqlConnection Connection() {
+        if (connection == null) {
+            connection = new MySqlConnection(constr);
+        }
+        return connection;
+    }
+
+    //打开数据库连接
+    public void OpenConnection() {
+        if (connection.State == ConnectionState.Closed) {
+            connection.Open();
+        } else if (connection.State == ConnectionState.Broken) {
+            connection.Close();
+            connection.Open();
+        }
+    }
+
+    //关闭数据库连接
+    public void CloseConnection() {
+        if (connection.State == ConnectionState.Open || connection.State == ConnectionState.Broken) {
+            connection.Close();
+        }
+    }
+}
---
+++ App_Code/Database/MysqlOperate.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// 实现数据的增删该查
+/// </summary>
+public class MysqlOperate
+{
+	/// <summary>
+    /// 实现insert delete update操作
+	/// </summary>
+	/// <param name="sqls">传入sql语句数组</param>
+	/// <returns></returns>
+    public Boolean IDU(String[] sqls) {
+        MySqlConnection con = null;
+        MysqlHelper mh = null;
+        MySqlCommand msc = null;
+        String sql = null;
+        try {
+            //数据库操作
+            con = MysqlHelper.Connection();
+            mh = new MysqlHelper();
+            mh.OpenConnection();
+            foreach (String s in sqls) {
+                sql += s;
+            }
+            msc = new MySqlCommand(sql, con);
+            msc.ExecuteNonQuery();
+        } catch (Exception) {
+            return false;
+        } finally {
+            mh.CloseConnection();
+        }
+        return true;
+    }
+
+    public List<Object> S(String sql) {
+        List<Object> datas = new List<Object>();
+        MySqlConnection con = MysqlHelper.Connection();
+        MysqlHelper mh = new MysqlHelper();
+        mh.OpenConnection();
+        MySqlCommand msc = new MySqlCommand(sql, con);
+        MySqlDataReader mdr = msc.ExecuteReader(CommandBehavior.CloseConnection);
+        while (mdr.Read()) {
+            //需要读取一行数据
+        }
+        mh.CloseConnection();
+        return datas;
+    }
+}
